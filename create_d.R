a3a76327b2807f9810b082afd3f367c0aa9c3fdd
rockclimber112358 <rockclimber112358@gmail.com>
2014-06-12 03:36:01 -0600

    Update create_d.R

--- create_d.R
+++ create_d.R
@@ -2,7 +2,6 @@ library(reshape)
 library(ggplot2)
 library(plyr)
 library(sqldf)
-library(bigmemory)
 
 setwd("C:/Users/rockc_000/Documents/Personal Files/Kaggle/Higgs Boson/")
 options(width=100)
@@ -30,5 +29,10 @@ for(i in phiCols)
   colnames(d)[ncol(d)+(-1):0] = paste0(c("sin_","cos_"),i)
   d[,i] = NULL
 }
+d = d[,c("EventId", "Weight", "Label", "Model_No", "cvGroup"
+    ,"DER_deltaeta_jet_jet", "DER_mass_jet_jet", "DER_prodeta_jet_jet", "DER_lep_eta_centrality", "PRI_jet_subleading_pt", "PRI_jet_subleading_eta", "sin_PRI_jet_subleading_phi", "cos_PRI_jet_subleading_phi"
+    ,"PRI_jet_leading_pt", "PRI_jet_leading_eta", "sin_PRI_jet_leading_phi", "cos_PRI_jet_leading_phi"
+    ,"DER_mass_MMC"
+    ,"DER_mass_transverse_met_lep", "DER_mass_vis", "DER_pt_h", "DER_deltar_tau_lep", "DER_pt_tot", "DER_sum_pt", "DER_pt_ratio_lep_tau", "DER_met_phi_centrality", "PRI_tau_pt", "PRI_tau_eta","PRI_lep_pt", "PRI_lep_eta", "PRI_met", "PRI_met_sumet", "PRI_jet_num", "PRI_jet_all_pt", "sin_PRI_tau_phi", "cos_PRI_tau_phi", "sin_PRI_lep_phi", "cos_PRI_lep_phi", "sin_PRI_met_phi", "cos_PRI_met_phi")]
 
 save(d, file="Data/finalData2.RData")
