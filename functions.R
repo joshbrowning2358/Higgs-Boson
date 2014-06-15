15bf2911f7471df44461b30910826008d2dceba2
rockclimber112358 <rockclimber112358@gmail.com>
2014-06-15 06:28:21 -0600

    Update functions.R

--- functions.R
+++ functions.R
@@ -13,6 +13,7 @@ if(Sys.info()[1]=="Windows")
   setwd("C:/Users/rockc_000/Documents/Personal Files/Kaggle/Higgs Boson/")
 if(Sys.info()[1]=="Linux")
   setwd("/media/storage/Personal Files/Kaggle/Higgs Boson/")
+source_github("https://raw.githubusercontent.com/rockclimber112358/Ensemble_Building_Code/master/cvModel.R")
 options(width=100)
 
 AMS = function(weight, act, pred){
